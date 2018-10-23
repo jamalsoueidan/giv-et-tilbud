171d120fa3425fc6529826a6dae83eb9bb6f4fd3
Jamal Soueidan <jamal@soueidan.com>
2018-10-23 13:54:31 +0200

    Add web application and build with create-react-app

---
+++ README.md
@@ -0,0 +1,7 @@
+Web Application and Api running on heroku.
+
+* node
+* hapi
+* react
+* redux
+* heroku
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Hapi = require('hapi');
 const jwt = require('jsonwebtoken');
+const Path = require('path');
+
 require('dotenv').load();
 
 const people = { // our "users database"
@@ -10,11 +12,9 @@ const people = { // our "users database"
 };
 
 const token = jwt.sign({ id: 1 }, process.env.SECRET_KEY, { algorithm: 'HS256'});
-console.log(token)
 
 // bring your own validation function
 const validate = async function (decoded, request) {
-  console.log(decoded)
     // do your checks to see if the person is valid
     if (!people[decoded.id]) {
       return { isValid: false };
@@ -24,43 +24,68 @@ const validate = async function (decoded, request) {
     }
 };
 
-const init = async () => {
-  const port = process.env.PORT || 8080;
-  const server = new Hapi.Server({ port: port });
-  // include our module here ↓↓
-  await server.register(require('hapi-auth-jwt2'));
+const routes = [
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true,
+      }
+    }
+  },
+  {
+    method: "GET", path: "/api", config: { auth: false },
+    handler: (request) => {
+      return {text: 'Token not required'};
+    }
+  },
+  {
+    method: 'GET', path: '/api/restricted', config: { auth: 'jwt' },
+    handler: (req, h) => {
+      const response = h.response({text: 'You used a Token!'});
+      response.header('Authorization', 'request.headers.authorization');
+      return response;
+    }
+  },
+  {
+    method: "POST", path: '/api/login', config: { auth: false },
+    handler: (req, h) => {
+      const payload = req.payload
+      return "Ok"
+    }
+  }
+]
 
-  server.auth.strategy('jwt', 'jwt',
-  { key: process.env.SECRET_KEY,          // Never Share your secret key
+const plugins = [
+  require('inert'),
+  require('hapi-auth-jwt2')
+]
+
+const init = async () => {
+  const port = process.env.PORT || 3000;
+  const server = new Hapi.Server({
+    port: port,
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'client', 'build')
+        }
+    }
+  });
+  // include our module here ↓↓
+  await server.register(plugins);
+
+  server.auth.strategy('jwt', 'jwt', {
+    key: process.env.SECRET_KEY,          // Never Share your secret key
     validate: validate,            // validate function defined above
     verifyOptions: { algorithms: [ 'HS256' ] } // pick a strong algorithm
   });
 
   server.auth.default('jwt');
-
-  server.route([
-    {
-      method: "GET", path: "/", config: { auth: false },
-      handler: (request) => {
-        return {text: 'Token not required'};
-      }
-    },
-    {
-      method: 'GET', path: '/restricted', config: { auth: 'jwt' },
-      handler: (req, h) => {
-        const response = h.response({text: 'You used a Token!'});
-        response.header('Authorization', 'request.headers.authorization');
-        return response;
-      }
-    },
-    {
-      method: "POST", path: '/data', config: { auth: false },
-      handler: (req, h) => {
-        const payload = req.payload
-        return "Ok"
-      }
-    }
-  ]);
+  server.route(routes);
   await server.start();
   return server;
 };
