bc42851e10395c257417a7a718ce216edb15e107
Jamal Soueidan <jamal@soueidan.com>
2018-11-02 23:37:50 +0100

    Don't show today if the shops already closed

--- shopify/src/pages/booking.js
+++ shopify/src/pages/booking.js
@@ -4,16 +4,31 @@ import moment from "moment";
 import classnames from "classnames";
 import { toggleProperty } from "../store";
 
+window.moment = moment;
+
 class PickDay extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      fromDate: props.selectedDay || moment()
+      fromDate: props.selectedDay || this.fromToday
     };
   }
 
+  get fromToday() {
+    const endOfDay = moment()
+      .set("minute", "00")
+      .set("hour", "20");
+
+    if (moment().isAfter(endOfDay)) {
+      return moment().add(1, "days");
+    }
+
+    return moment();
+  }
+
   back = () => {
-    const behind = moment(this.state.fromDate).isBefore(moment());
+    const behind = moment(this.state.fromDate).isBefore(this.fromToday);
     if (!behind) {
       this.setState({
         fromDate: moment(this.state.fromDate).subtract(5, "days")
@@ -37,11 +52,12 @@ class PickDay extends React.Component {
 
     let dates = [this.state.fromDate];
     for (let i = 1; i < 5; i++) {
-      dates.push(moment().add(i, "days"));
+      dates.push(moment(this.state.fromDate).add(i, "days"));
     }
 
     return (
       <div className="pickDay">
+        <h1>Vælge en day</h1>
         <div onClick={this.back}>Back</div>
         {dates.map(day => (
           <div
@@ -65,6 +81,8 @@ class Book extends React.Component {
   onDay = day => {
     this.props.toggleProperty("datetime", day.format());
   };
+
+  onTime = time => {};
 
   render() {
     const datetime = this.props.datetime ? this.props.datetime.value : null;
