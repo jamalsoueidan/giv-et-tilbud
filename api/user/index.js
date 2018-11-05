228af9edfe9d136ca70f813cfa3eeb1e397bce17
Jamal Soueidan <jamal@soueidan.com>
2018-11-05 15:09:38 +0100

    Update delete workshop api

--- api/user/index.js
+++ api/user/index.js
@@ -62,23 +62,15 @@ module.exports = [
       const { workshopId } = req.params;
       const credentials = req.auth.credentials;
 
-      const user = await User.findOne({
-        _id: credentials.id
-      }).select({ password: 0 });
-
-      // should we care?
-      if (
-        !user.workshops.some(workshop => String(workshop._id) !== workshopId)
-      ) {
-        return Boom.badData("No workshop with this id");
-      }
-
-      user.workshops = user.workshops.filter(
-        workshop => String(workshop._id) !== workshopId
-      );
-      user.save();
-
-      return user;
+      return await User.findOneAndUpdate(
+        { _id: credentials.id },
+        {
+          $pull: { workshops: { _id: workshopId } }
+        },
+        {
+          new: true
+        }
+      ).select({ password: 0 });
     },
     options: {
       validate: {
