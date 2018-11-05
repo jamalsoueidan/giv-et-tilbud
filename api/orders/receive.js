ca09a8aae65cce8eedd35ff5f61aa5e5273f255c
Jamal Soueidan <jamal@soueidan.com>
2018-11-05 17:34:35 +0100

    Get orders now response with near shop location

--- api/orders/receive.js
+++ api/orders/receive.js
@@ -1,8 +1,23 @@
 const rp = require("request-promise");
 const Order = require("../../models/order");
+const User = require("../../models/user");
+const Boom = require("boom");
 
 module.exports = async req => {
   const payload = req.payload;
+  const credentials = req.auth.credentials;
+  const workshopId = req.query.workshopid;
+
+  const workshops = await User.find(
+    { _id: credentials.id, "workshops._id": workshopId },
+    { _id: 0, "workshops.$": 1 }
+  );
+
+  if (workshops.length === 0) {
+    return Boom.badData("Workshop id doesn't exist!");
+  }
+
+  const coordinates = workshops[0].workshops[0].location.coordinates;
 
   //https://stackoverflow.com/questions/5681851/mongodb-combine-data-from-multiple-collections-into-one-how
   const orders = await Order.aggregate([
@@ -10,7 +25,7 @@ module.exports = async req => {
       $geoNear: {
         near: {
           type: "Point",
-          coordinates: [10.131774, 56.159447]
+          coordinates: coordinates
         },
         distanceField: "distance",
         spherical: true,
