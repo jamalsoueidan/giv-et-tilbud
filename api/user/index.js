400227069bfcdef5423fd8908e2505aa95fbc48d
Jamal Soueidan <jamal@soueidan.com>
2018-11-09 18:52:21 +0100

    Add autocomplete for full address on the shopify mini app, using dawa api

--- api/user/index.js
+++ api/user/index.js
@@ -8,6 +8,52 @@ const JWT = require("jsonwebtoken");
 module.exports = [
   {
     method: "GET",
+    path: "/api/workshops",
+    handler: async req => {
+      //https://stackoverflow.com/questions/19266590/merging-array-fields-in-mongodb-aggregation
+      try {
+        const workshops = await User.aggregate([
+          {
+            $group: { _id: "workshops", workshops: { $push: "$workshops" } }
+          },
+
+          { $unwind: "$workshops" },
+          { $unwind: "$workshops" },
+          {
+            $group: {
+              _id: "$_id",
+              workshops: {
+                $addToSet: "$workshops"
+              }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              "workshops._id": 0,
+              "workshops.created_at": 0,
+              "workshops.phone": 0,
+              "workshops.email": 0,
+              "workshops.location.type": 0
+            }
+          }
+        ]);
+
+        if (workshops.length > 0) {
+          return workshops[0].workshops;
+        } else {
+          return [];
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    options: {
+      auth: false
+    }
+  },
+  {
+    method: "GET",
     path: "/api/user",
     handler: async req => {
       const credentials = req.auth.credentials;
