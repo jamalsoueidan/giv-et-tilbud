0a3b6531d5ebc47ca79b2155c6fefeb1e0f208ef
Jamal Soueidan <jamal@soueidan.com>
2018-11-11 19:03:10 +0100

    Load shopify applications dynamically

--- shopify/src/index.js
+++ shopify/src/index.js
@@ -1,11 +1,9 @@
-/*import createOrderRender from "./create_order";
 const createOrder = document.getElementById("create_order");
 if (createOrder) {
-  createOrderRender(createOrder);
-}*/
+  import("./create_order").then(module => module.default(createOrder));
+}
 
-import viewOffersRender from "./view_offers";
 const viewOffers = document.getElementById("view_offers");
 if (viewOffers) {
-  viewOffersRender(viewOffers);
+  import("./view_offers").then(module => module.default(viewOffers));
 }
